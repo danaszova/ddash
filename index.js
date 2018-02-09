56cd345526aedebb89c0678348379fae5821a794
Dana Szova <dana.s.szova@gmail.com>
2018-02-08 21:04:00 -0800

    adds geocoder

--- index.js
+++ index.js
@@ -20,7 +20,23 @@ exports.nowDate = (start, end) => {
 };
 
 exports.getCount = () => {
-  const count = Math.random();
-  console.log("Welcome to the get count ", count);
+  const count = Math.floor(Math.random() * 20000);
   return count;
+};
+
+exports.gecoder = (ln, cb, key) => {
+  const { address, city, state } = ln;
+  const protocol = "https://";
+  const url = address + "," + city + "," + state;
+  const api = "maps.googleapis.com/maps/api/geocode/json?address=";
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      const location = JSON.parse(xhr.responseText).results[0].geometry
+        .location;
+      cb(location);
+    }
+  };
+  xhr.open("GET", protocol + api + url + key, true);
+  https: xhr.send(null);
 };
